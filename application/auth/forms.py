0382c0680b36d574940b114cb033e74cc40015ea
LauriTahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2019-06-14 10:29:40 +0300

    Change errormessage visuals.

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators, IntegerField
+from datetime import date
 
 class LoginForm(FlaskForm):
     username = StringField("Käyttäjänimi")
@@ -12,15 +13,15 @@ class CreateUserForm(FlaskForm):
     username = StringField("Käyttäjänimi", [
         validators.Length(min=4, max=16, message='Käyttäjänimen on oltava pituudeltaan 4-16 merkkiä!'),
         validators.input_required(message='Käyttäjänimi ei voi olla tyhjä!')
-    ])
+    ], render_kw={"placeholder": "Min. 4, maks. 16 merkkiä"})
     password = PasswordField("Salasana", [
         validators.Length(min=8, max=30, message='Salasanan on oltava pituudeltaan 8-30 merkkiä!'),
         validators.equal_to('confirmPwd', message='Salasanojen on oltava samat!')
-    ])
+    ], render_kw={"placeholder": "Min. 8, maks. 30 merkkiä"})
     confirmPwd = PasswordField("Vahvista salasana")
-    name = StringField("Nimi", [validators.input_required(message='Nimi ei voi olla tyhjä!')])
-    city = StringField("Kaupunki")
-    age = IntegerField("Syntymävuosi", [validators.number_range(min=1888, max=2012)])
+    name = StringField("Nimi", [validators.input_required(message='Nimi ei voi olla tyhjä!')], render_kw={"placeholder": "Matti Meikäläinen"})
+    city = StringField("Kaupunki", render_kw={"placeholder": "Nurmes"})
+    age = IntegerField("Syntymävuosi", [validators.number_range(min=int(date.today().year) - 125, max=int(date.today().year), message=("Syntymävuoden on oltava välillä " + str((int(date.today().year) - 125)) + " ja " + str(int(date.today().year))) + "!")], render_kw={"placeholder": "1982"})
 
     class Meta:
         csrf = False
@@ -30,7 +31,7 @@ class ChangePasswordForm(FlaskForm):
     password = PasswordField("Uusi salasana", [
         validators.Length(min=8, max=30, message='Salasanan on oltava pituudeltaan 8-30 merkkiä!'),
         validators.equal_to('confirmPassword', message='Salasanojen on oltava samat!')
-    ])
+    ], render_kw={"placeholder": "Min. 8, maks. 30 merkkiä"})
     confirmPassword = PasswordField("Vahvista uusi salasana")
 
     class Meta:
